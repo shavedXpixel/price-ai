a51e56d791ff76f0fedb8c1e3eec534bab99b11f
PRIYANSU DASH <100258436+Priyanshu-UI123@users.noreply.github.com>
2026-02-01 02:15:15 +0530

    Revert "hide"
    This reverts commit 7b9d7513cc71403d87399b040682c24673ff799f.

--- frontend/app/firebase.ts
+++ frontend/app/firebase.ts
@@ -1,16 +1,18 @@
+// frontend/app/firebase.ts
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: "AIzaSyB3S3aEHMtejoiN3cySSv6P8KcxHPdxK5s",
+  authDomain: "priceai-390a2.firebaseapp.com",
+  projectId: "priceai-390a2",
+  storageBucket: "priceai-390a2.firebasestorage.app",
+  messagingSenderId: "1013278530259",
+  appId: "1:1013278530259:web:e3f5b89b0ed7fe74999226"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
